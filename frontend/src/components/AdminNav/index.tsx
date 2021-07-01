ad2a3385d8d5adf1144742b61735de1b6a353ef9
Jehyun <congmul@hotmail.com>
2021-07-01 00:15:44 -0700

    Add function for modilbe menu

--- frontend/src/components/AdminNav/index.tsx
+++ frontend/src/components/AdminNav/index.tsx
@@ -1,11 +1,30 @@
 const AdminNav: React.FC = () => {
 
+    const mobileNavClick = () => {
+        // The exclamation mark means a developer knows there is a button element.( The value is not null )
+        const mobileLinksEl = document.getElementById('mobile-Links')!;
+
+        if (mobileLinksEl.style.display === "block") {
+            mobileLinksEl.style.display = "none";
+            
+          } else {
+            mobileLinksEl.style.display = "block";
+          }
+    }
+
     return (
         <header className="adminNav-header">
         <h5>Amin Page</h5>
         <nav className="adminNav-mobile">
-            <a href="#test"><i className="fas fa-bars"></i></a>
+            <a href="#test" id="mobile-menu" onClick={mobileNavClick}><i className="fas fa-bars"></i></a>
         </nav>
+        <div id="mobile-Links">
+                <a href="#mobile-Links01">Marketing</a>
+                <a href="#mobile-Links02">Add New Inventory</a>
+                <a href="#mobile-Links03">Order history</a>
+                <a href="#mobile-Links04">Messages</a>
+                <a href="#mobile-Links05">Print reports</a>
+            </div>
         <nav className="adminNav-desktop">
             <ul>
                 <li>
