5f2e2ae38b67a2a11e89333119b99e2a0cce686e
Amber Lee <lee.amber.alex@gmail.com>
2021-07-02 12:44:36 -0700

    Fixed mixins for buttons and form.

--- frontend/src/pages/DashContainer.tsx
+++ frontend/src/pages/DashContainer.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Login from '../components/Login';
 
+import Login from '../components/Login';
+import "../sass/4_layout/_forms.scss"
 
 
 export default function DashContainer(){
