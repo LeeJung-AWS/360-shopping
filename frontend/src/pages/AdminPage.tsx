a01157fad813bd894f1ae755eabea654807652e1
Jehyun <congmul@hotmail.com>
2021-07-03 20:55:34 -0700

    Layout for admin-page

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -3,6 +3,14 @@
 import AdminNav from '../components/AdminNav';
 
 const AdminPage: React.FC = () => {
+    const amdinTabMenuClick = () => {
+        // The exclamation mark means a developer knows there is a button element.( The value is not null )
+        const adminTablinks = document.querySelectorAll('.admin-tablinks')!;
+        console.log(this);
+        console.log(adminTablinks);
+
+    }
+
 
     return <>
         <AdminNav />
@@ -11,11 +19,34 @@ const AdminPage: React.FC = () => {
                 <div className="card-header">
                     <h4>Sales</h4>
                 </div>
-                <div className="card-body">
-                    <p>Tab Button</p>
-                    <p>Graph</p>
-                    <p>Graph</p>
-                    <p>Graph</p>
+                <div className="admin-tab-card-body">
+                    <div className="admin-tab">
+                        <button className="admin-tablinks" autoFocus onClick={amdinTabMenuClick}>REVENUE</button>
+                        <button className="admin-tablinks" onClick={amdinTabMenuClick}>UNITS SOLD</button>
+                        <button className="admin-tablinks" onClick={amdinTabMenuClick}>ORDERS</button>
+                        <button className="admin-tablinks" onClick={amdinTabMenuClick}>AOV</button>
+                    </div>
+                    <div className="admin-tab-body">
+                        <div id="revenue-tab" className="tabcontent">
+                            <p>Revenue Graph</p>
+                            <p>Revenue Graph</p>
+                            <p>Revenue Graph</p>
+                            {/* <p>Revenue Graph</p>
+                            <p>Revenue Graph</p>
+                            <p>Revenue Graph</p>
+                            <p>Revenue Graph</p>
+                            <p>Revenue Graph</p> */}
+                        </div>
+                        <div id="units-sold-tab" className="tabcontent">
+                            <p>Units Sold Graph</p>
+                        </div>
+                        <div id="orders-tab" className="tabcontent">
+                            <p>Orders Graph</p>
+                        </div>
+                        <div id="aov-tab" className="tabcontent">
+                            <p>AOV Graph</p>
+                        </div>
+                    </div>
                     {/* <p>Graph</p>
                     <p>Graph</p>
                     <p>Graph</p>
