e72de189d2df0eaebd16218b9a8d65603f8383ce
Amber Lee <lee.amber.alex@gmail.com>
2021-07-09 14:11:06 -0700

    Removed bootstrap link.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/main.scss';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
