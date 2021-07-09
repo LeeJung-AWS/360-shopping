8588cbbad2710ac9c81de466eee83d43805eb276
Amber Lee <lee.amber.alex@gmail.com>
2021-07-09 14:12:41 -0700

    Structure for search items.

---
+++ frontend/src/components/DashShopItems1/index.tsx
@@ -0,0 +1,19 @@
+const DashShopItems: React.FC = () => {
+
+  return (
+    <>
+      <section className="grid-container">
+          {/* will replace with links to search items */}
+        <div className="grid-item">Womens</div>
+        <div className="grid-item">Mens</div>
+        <div className="grid-item">Fluid</div>
+        <div className="grid-item">Boys</div>
+        <div className="grid-item">Girls</div>
+        <div className="grid-item">Babies</div>
+        
+      </section>
+    </>
+  );
+};
+
+export default DashShopItems;
