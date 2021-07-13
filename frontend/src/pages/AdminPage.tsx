da548fbfbc3045d0e020b354cc3a09f9d08aebe0
Jehyun <congmul@hotmail.com>
2021-07-13 16:42:22 -0700

    Add Router Switch

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import AdminNav from '../components/AdminNav';
 import Tabmenu from '../components/TabMenu';
 import ContentsTopMenu from '../components/TabMenu/ContentsTopMenu';
@@ -26,63 +28,78 @@ const AdminPage: React.FC = () => {
         yAixsTickprefix: '$'
     }
 
+    const dateRange = 'Jan 01 ~ Dec 31, 2021';
+
     const tabMenuInformation = [
         { 
             title: 'REVENUE',
             secondRow: 23400,
             secondRowUnit: '$',
-            contentsTopMenu: <ContentsTopMenu title={'Revenue'} rangeDate={'Jan 01 ~ Dec 31, 2021'} secondRowUnit={'$'} secondRow={23400} />,
+            contentsTopMenu: <ContentsTopMenu title={'Revenue'} rangeDate={dateRange} secondRowUnit={'$'} secondRow={23400} />,
             contents: revenueContents,
         },
         { 
             title: 'UNITS SOLD',
             secondRow: 74,
-            contentsTopMenu: <ContentsTopMenu title={'Units Sold'} rangeDate={'Jan 01 ~ Dec 31, 2021'} secondRow={74}/>,
+            contentsTopMenu: <ContentsTopMenu title={'Units Sold'} rangeDate={dateRange} secondRow={74}/>,
             contents: unitSoldContents,
 
         },
         { 
             title: 'ORDERS',
             secondRow: 47,
-            contentsTopMenu: <ContentsTopMenu title={'Orders'} rangeDate={'Jan 01 ~ Dec 31, 2021'} secondRow={47} />,
+            contentsTopMenu: <ContentsTopMenu title={'Orders'} rangeDate={dateRange} secondRow={47} />,
             contents: ordersContents,
         },
         { 
             title: 'AOV',
             secondRow: 350,
             secondRowUnit: '$',
-            contentsTopMenu: <ContentsTopMenu title={'Average Order Value'} rangeDate={'Jan 01 ~ Dec 31, 2021'} secondRowUnit={'$'} secondRow={350} />,
+            contentsTopMenu: <ContentsTopMenu title={'Average Order Value'} rangeDate={dateRange} secondRowUnit={'$'} secondRow={350} />,
             contents: aovContents,
         }
     ]
 
-    return <>
-        <AdminNav />
-        
-        <main className="adminPage-row">
-            <Tabmenu header="Sales" tabMenuInformation={tabMenuInformation}/>
-
-            <section className="adminPage-col card">
-                <div className="card-header">
-                    <h4>Awaiting Shipments</h4>
-                </div>
-                <div className="card-body">
-                    <p>Shipments information</p>
-                </div>
-            </section>
-            <section className="adminPage-col card">
-                <div className="card-header">
-                    <h4>Messages</h4>
-                </div>
-                <div className="card-body">
-                    <p>Messages</p>
-                    <p>Messages</p>
-                    <p>Messages</p>
-                </div>
-            </section>
-        </main>
-
-        </>
+    return <Router>
+            <AdminNav />
+            <Switch>
+                <Route exact path="/adminPage">
+                    <main className="adminPage-row">
+                        <Tabmenu header="Sales" tabMenuInformation={tabMenuInformation}/>
+                        <section className="adminPage-col card">
+                            <div className="card-header">
+                                <h4>Awaiting Shipments</h4>
+                            </div>
+                            <div className="card-body">
+                                <p>Shipments information</p>
+                            </div>
+                        </section>
+                        <section className="adminPage-col card">
+                            <div className="card-header">
+                                <h4>Messages</h4>
+                            </div>
+                            <div className="card-body">
+                                <p>Messages</p>
+                                <p>Messages</p>
+                                <p>Messages</p>
+                            </div>
+                        </section>
+                    </main>
+                </Route>
+                <Route exact path="/adminPage/marketing">
+                    <p>Marketing Page</p>
+                </Route>
+                <Route exact path="/adminPage/addNewInventory">
+                    <p>addNewInventory Page</p>
+                </Route>
+                <Route exact path="/adminPage/orderHistory">
+                    <p>orderHistory Page</p>
+                </Route>
+                <Route exact path="/adminPage/printReports">
+                    <p>printReports Page</p>
+                </Route>
+            </Switch>
+        </Router>
 }
 
 export default AdminPage;
