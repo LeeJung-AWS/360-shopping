9b8fb8ba2e7c264cca5a37a9ce74eb50e82ea319
Jehyun <congmul@hotmail.com>
2021-07-17 23:22:17 -0700

    Add Layout and Style for Title Description

--- frontend/src/components/AddNewProduct/index.tsx
+++ frontend/src/components/AddNewProduct/index.tsx
@@ -1,4 +1,28 @@
+import { useState } from 'react';
+
 const AddNewProduct: React.FC = () => {
+    const [addNewProductTitle, setAddNewProductTitle] = useState('');
+    const [addNewProductDescription, setAddNewProductDescription] = useState('');
+
+    // const onFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     console.log(e.target.className)
+    //     if(e.target.className === 'addNewProduct-title' && addNewProductTitle === 'Add Product Name'){
+    //         setAddNewProductTitle('');
+    //     }
+
+    //     if(e.target.className === 'addNewProduct-description' && addNewProductDescription === 'Add description...'){
+    //         setAddNewProductDescription('');
+    //     }
+    // }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.className === 'addNewProduct-title'){
+            setAddNewProductTitle(e.target.value);
+        }
+        if(e.target.className === 'addNewProduct-description'){
+            setAddNewProductDescription(e.target.value);
+        }
+    }
 
     return (
         <div className="card">
@@ -8,12 +32,31 @@ const AddNewProduct: React.FC = () => {
                 <button>Cancel</button>
             </div>
             <div className="card-body addNewProduct-body">
-                <div className="addNewProduct-title">Add Product Name</div>
-                <div className="addNewProduct-title">Add description...</div>
-                <div>IMG BLOCK</div>
-                <div>Pricing</div>
-                <div>Inventory</div>
-                <div>Categories</div>
+                <div className="addNewProduct-body-item">
+                    <input onChange={onChange} className="addNewProduct-title" placeholder="Add Product Name" value={addNewProductTitle} />
+                </div>
+                <div className="addNewProduct-body-item">
+                    <input onChange={onChange} className="addNewProduct-description" placeholder="Add description..." value={addNewProductDescription} />
+                </div>
+                <div className="addNewProduct-body-item addNewProduct-img-box">
+                    <div></div>
+                    <div>ADD IMAGES</div>
+                </div>
+                <div className="addNewProduct-body-item">
+                    <p>Pricing</p>
+                    <div>Price</div>
+                    <div>On Sale</div>
+                    <div>Sale Price</div>
+                </div>
+                <div  className="addNewProduct-body-item">
+                    <p>Inventory</p>
+                    <div>Quantity</div>
+                    <div>SKU</div>
+                </div>
+                <div  className="addNewProduct-body-item">
+                    <p>Categories</p>
+                    <button>Add...</button>
+                </div>
             </div>
         </div>
     )
