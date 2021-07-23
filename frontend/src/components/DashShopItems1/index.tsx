9847ef4ef80bb547acd05d48f2006ed23404333f
Amber Lee <lee.amber.alex@gmail.com>
2021-07-23 13:21:18 -0700

    Fixed responsiveness on shop  items.

--- frontend/src/components/DashShopItems1/index.tsx
+++ frontend/src/components/DashShopItems1/index.tsx
@@ -1,16 +1,56 @@
+import prod1 from "../../img/prod1.png";
+import prod2 from "../../img/prod2.png";
+import prod3 from "../../img/prod3.png";
+
 const DashShopItems: React.FC = () => {
+
+  const displayCategories = () => {
+    // let category = e.target.value;
+    // switch(category) {
+    //   case "Womens":
+    //     showWomens();
+    //     break;
+    // };
+  };
+
+  // API calls to the db?
+  const showWomens = () => {};
 
   return (
     <>
       <section className="grid-container">
-          {/* will replace with links to search items */}
-        <div className="grid-item">Womens</div>
-        <div className="grid-item">Mens</div>
-        <div className="grid-item">Fluid</div>
-        <div className="grid-item">Boys</div>
-        <div className="grid-item">Girls</div>
-        <div className="grid-item">Babies</div>
-        
+        {/* will replace with links to search items */}
+        <div onClick={displayCategories} className="grid-item womens">
+          Womens
+        </div>
+        <div onClick={displayCategories} className="grid-item">
+          Mens
+        </div>
+        <div onClick={displayCategories} className="grid-item">
+          Fluid
+        </div>
+        <div onClick={displayCategories} className="grid-item">
+          Boys
+        </div>
+        <div onClick={displayCategories} className="grid-item">
+          Girls
+        </div>
+        <div onClick={displayCategories} className="grid-item">
+          Babies
+        </div>
+      </section>
+
+      <section className="grid-container2">
+        {/* will replace with links to search items */}
+        <div className="grid-item2 img-dashshop">
+          <img className="img-dashshop"  src={prod1} />
+        </div>
+        <div className="grid-item2 ">
+          <img className="img-dashshop" src={prod2} />
+        </div>
+        <div className="grid-item2 img-dashshop">
+          <img className="img-dashshop"  src={prod3} />
+        </div>
       </section>
     </>
   );
