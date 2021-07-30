9d6494195bba7d5307ccfde3dbd0e1e5d54320b4
Amber Lee <lee.amber.alex@gmail.com>
2021-07-30 10:53:27 -0700

    Removed dashnav.

--- frontend/src/pages/DashContainer.tsx
+++ frontend/src/pages/DashContainer.tsx
@@ -6,12 +6,10 @@ import DashShopItems1 from "../components/DashShopItems1";
 export const DashContainer: React.FC = () => {
   return (
     <>
-      <DashNav />
-
+     
       <Carousel />
 
       <DashShopItems1 />
-
     </>
   );
 };
