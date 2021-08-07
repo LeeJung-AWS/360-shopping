c6f67c622fdfe4730d82f1710c2494633cfea907
Jehyun <congmul@hotmail.com>
2021-08-07 00:03:09 -0700

    Set Models

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,20 +1,28 @@
 import { Schema, model } from 'mongoose';
 
-interface Usertype{
+interface UserType{
     name: string;
+    username: string;
     email: string;
     password: string;
+    address: string;
+    phone: string;
+    userCreated: Date;
 }
 
 // Create a Schema corresponding to the document interface.
-const UserSchema = new Schema<Usertype>({
+const UserSchema = new Schema<UserType>({
   name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  address: { type: String, required: true },
+  phone: { type: String, required: false },
+  userCreated: { type: Date, default: Date.now }
 });
 
 
 
-const User = model<Usertype>('User', UserSchema);
+const User = model<UserType>('User', UserSchema);
 
 export default User;
