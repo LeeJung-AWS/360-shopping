caddf9487f026fa25e7f4c5643ad54e4d98ef79d
Jehyun <congmul@hotmail.com>
2021-08-21 22:42:43 -0700

    Modified promps

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,49 +5,50 @@ import { Link } from "react-router-dom";
 interface NavbarDatatype {
     header: string;
     links: string[];
+    linkTitle: string[];
 }
 
-const Navbar: React.FC<NavbarDatatype> = ({header, links}) => {
+const Navbar: React.FC<NavbarDatatype> = ({header, links, linkTitle}) => {
 
     // In Mobile Size, display or hide Navigation Menu
     const mobileNavClick = () => {
-        const mobileLinksEl = document.getElementById('mobile-Links')!;  // The exclamation mark means a developer knows there is a button element.( The value is not null )
-        // console.log(mobileLinksEl);
-        if (mobileLinksEl.style.height === '100%') {
-            mobileLinksEl.style.height = '0';
-            mobileLinksEl.style.padding = '0';
+        const navBarmobileLinksEl = document.getElementById('navBar-mobile-Links')!;  // The exclamation mark means a developer knows there is a button element.( The value is not null )
+        // console.log(navBarmobileLinksEl);
+        if (navBarmobileLinksEl.style.height === '100%') {
+            navBarmobileLinksEl.style.height = '0';
+            navBarmobileLinksEl.style.padding = '0';
           } else {
-            mobileLinksEl.style.height = '100%';
-            mobileLinksEl.style.padding = '6rem 2rem';
+            navBarmobileLinksEl.style.height = '100%';
+            navBarmobileLinksEl.style.padding = '6rem 2rem';
           }
         
     }
 
     function onclickMenu () {
-        const mobileLinksEl = document.getElementById('mobile-Links')!;
-        mobileLinksEl.style.height = '0';
-        mobileLinksEl.style.padding = '0';
+        const navBarmobileLinksEl = document.getElementById('navBar-mobile-Links')!;
+        navBarmobileLinksEl.style.height = '0';
+        navBarmobileLinksEl.style.padding = '0';
     }
 
     return (
     <header className="navBar-header">
-        <Link id="admin-page-btn" to="/">{header}</Link>
+        <Link id="navBar-page-btn" to="/">{header}</Link>
         <nav className="navBar-mobile">
             <a href="#navBar" id="mobile-menu" onClick={mobileNavClick}><i className="fas fa-bars"></i></a>
         </nav>
-        <div id="mobile-Links">
-            {links.map(link => {
+        <div id="navBar-mobile-Links">
+            {links.map((link, index) => {
                 return(
-                    <Link to={`/${link}`} onClick={onclickMenu}>{link}</Link>
+                    <Link to={`/${link}`} onClick={onclickMenu}>{linkTitle[index]}</Link>
                 )
             })}
         </div>
         <nav className="navBar-desktop">
             <ul>
-                {links.map(link => {
+                {links.map((link, index) => {
                     return(
                         <li>
-                            <Link to={`/${link}`}>{link}</Link>
+                            <Link to={`/${link}`} onClick={onclickMenu}>{linkTitle[index]}</Link>
                         </li>
                     )
                 })}
