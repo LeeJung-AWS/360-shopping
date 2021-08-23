1f2e95e04bfdde998415a7c7c8b40793e4c4b34e
Jehyun <congmul@hotmail.com>
2021-08-23 16:55:35 -0700

    Add User routes

--- backend/src/routes/api/users.ts
+++ backend/src/routes/api/users.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'; 
-import { userCreate } from '../../controllers/users';
+import { getSingleUser, userCreate, login } from '../../controllers/users';
 
 const router = Router();
+// import middleware
+import { authMiddleware } from '../../libs/auth';
 
 // Location:  /api/user/
-router.post("/", userCreate);
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(userCreate).put(authMiddleware);
+router.route('/login').post(login);
+router.route('/me').get(authMiddleware, getSingleUser);
 
 export default router;
