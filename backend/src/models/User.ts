ae1b3955e3c470800633eedcce51cd8b25be98df
Jehyun <congmul@hotmail.com>
2021-08-23 07:08:51 -0700

    Modified User model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -12,11 +12,11 @@ interface UserType{
 
 // Create a Schema corresponding to the document interface.
 const UserSchema = new Schema<UserType>({
-  name: { type: String, required: "Name is Required" },
+  name: { type: String, required: false },
   username: { type: String, required: "Username is Required", unique: true },
   email: { type: String, match: [/.+@.+\..+/, "Please enter a valid e-mail address"], unique: true },
   password: { type: String, required: "password is Required" },
-  address: { type: String, required: "address is Required" },
+  address: { type: String, required: false },
   phone: { type: String, required: false },
   userCreated: { type: Date, default: Date.now }
 });
