ad40b15ee5d5d89ce4fb56b6ae0b42698308ecd2
Jehyun <congmul@hotmail.com>
2021-08-25 12:29:52 -0700

    Update styling for displayProduct component

--- frontend/src/components/DisplayProducts/index.tsx
+++ frontend/src/components/DisplayProducts/index.tsx
@@ -5,8 +5,11 @@ const DisplayProducts: React.FC = () => {
         <section className="products-lists-container">
             <section className="products-lists">
                 <div className="product-item">
-                    <img src="https://img.ltwebstatic.com/images3_pi/2021/07/02/16252200307d3f3d099403e9e38a725fc0f62720b7.webp" alt="product" />
-                    <div>title</div>
+                    <div className="product-item-top">
+                        <img src="https://img.ltwebstatic.com/images3_pi/2021/07/02/16252200307d3f3d099403e9e38a725fc0f62720b7.webp" alt="product" />
+                        <button className="product-item-top-btn">ADD TO CART</button>
+                        <div className="product-item-top-title">titletitletitletitletitletitletitletitletitletitle</div>
+                    </div>
                     <div className="product-item-bottom">
                         <div className="product-item-bottom-price">US$14.99</div>
                         <div className="product-item-bottom-icon">
